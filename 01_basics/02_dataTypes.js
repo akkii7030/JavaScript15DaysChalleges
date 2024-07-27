ad79a6bfc97956fa79926826a03cdcff2dcb6db0
Vishwakarma Akhilesh Lalji <96793108+akkii7030@users.noreply.github.com>
2024-07-27 07:09:27 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+let name = "Akhilesh";  // String
+let num ="123";        // Numbers
+let val= null;          // Standalone value
+let x;                  // Undefined
+let isCorrect = true ;  // Boolean
+                        // Symbols
+
+const person = {firstName:"John", lastName:"Doe"}; // Objects
