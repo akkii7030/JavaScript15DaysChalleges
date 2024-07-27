57b1836f37fe04ad38caeadb408e36ce8691bfd6
Vishwakarma Akhilesh Lalji <96793108+akkii7030@users.noreply.github.com>
2024-07-27 07:09:14 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = "1234";
+let accoutEmail = "ak@gmail.com";
+var accountPassword = "4321";
+accountCity = "Mumbai"
+let accountState;
+console.log(accountId)
+
+// accountId = "2345"  const not allow us to reassign the variable value
+// Prefer not to use var because of issue in block scope and functional scope
+
+accoutEmail = "av@gmail.com";
+accountPassword = "5678";
+accountCity = "UttarPradesh"
+console.log([accoutEmail,accountPassword,accountCity, accountState
+])
